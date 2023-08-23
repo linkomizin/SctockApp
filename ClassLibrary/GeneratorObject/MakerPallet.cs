334c15819930d074f7459e36eeafe2d37537d645
linkomizin <linkomizin@gmail.com>
2023-08-23 21:21:18 +0300

    fix naming

--- ClassLibrary/GeneratorObject/MakerPallet.cs
+++ ClassLibrary/GeneratorObject/MakerPallet.cs
@@ -15,20 +15,20 @@ namespace ClassLibrary.GeneratorObject
             _random = new Random();
         }
 
-        public List<Pallet> MakePallets(int totalCount, double minimalNum, double maxH, double maxW, double maxL, double standartScale = 30)
+        public List<Pallet> MakePallets(int totalCount, double minimalNum, double maxH, double maxW, double maxL, double standardScale = 30)
         {
             List<Pallet> pallets = new List<Pallet>();
 
             for (int i = 0; i < totalCount; i++)
             {
-                var item = MakePallet(minimalNum, maxH, maxW, maxL, standartScale);
+                var item = MakePallet(minimalNum, maxH, maxW, maxL, standardScale);
                 pallets.Add(item);
             }
 
             return pallets;
         }
 
-        private Pallet MakePallet(double minimalNum, double maxH, double maxW, double maxL, double standartScale)
+        private Pallet MakePallet(double minimalNum, double maxH, double maxW, double maxL, double standardScale)
         {
             return new Pallet(
                 id: Guid.NewGuid().ToString(),
