14ba37f732e5ad84c7c9d4b2a486958b88c04fbd
linkomizin <linkomizin@gmail.com>
2023-08-23 21:26:52 +0300

    hmmmm

---
+++ ClassLibrary/Worker/WorkerStock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.GeneratorObject;
+using ClassLibrary.Model;
+
+namespace ClassLibrary.Worker
+{
+    public class WorkerStock
+    {
+        private MakerBox _makerBox;
+        private MakerPallet _makerPallet;
+
+        private List<Box> _boxList;
+        private List<Pallet> _palletList;
+
+        private 
+
+
+    }
+}
